5392412833230144cb416a31356a6be0603160c5
LinChaoqunRick <602314134@qq.com>
2023-12-03 21:19:31 +0800

    build: 使用容器名称访问

--- auth-service/src/main/java/com/youyu/AuthServiceApp.java
+++ auth-service/src/main/java/com/youyu/AuthServiceApp.java
@@ -2,10 +2,12 @@ package com.youyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RefreshScope
 public class AuthServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApp.class, args);
