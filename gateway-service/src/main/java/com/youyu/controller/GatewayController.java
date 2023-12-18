61d9928da3efba21d812f7718e4460763a6b1bce
LinChaoqunRick <602314134@qq.com>
2023-12-18 17:58:33 +0800

    build: 配置方式修改

--- gateway-service/src/main/java/com/youyu/controller/GatewayController.java
+++ gateway-service/src/main/java/com/youyu/controller/GatewayController.java
@@ -1,13 +1,22 @@
 package com.youyu.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RefreshScope
 public class GatewayController {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
 
     @GetMapping("/gateway/test")
     public String gatewayTest() {
-        return "/gateway/test";
+        log.info(datasourceUrl);
+        return "/gateway/test" + datasourceUrl;
     }
 }
