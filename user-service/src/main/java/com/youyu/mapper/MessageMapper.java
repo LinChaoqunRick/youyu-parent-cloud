305c4f00416f33c59beb89f01eed55ae961b8fae
LinChaoqunRick <602314134@qq.com>
2024-02-21 22:20:43 +0800

    feat: 留言

--- user-service/src/main/java/com/youyu/mapper/MessageMapper.java
+++ user-service/src/main/java/com/youyu/mapper/MessageMapper.java
@@ -2,6 +2,8 @@ package com.youyu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.youyu.entity.user.Message;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * (Message)表数据库访问层
@@ -9,6 +11,8 @@ import com.youyu.entity.user.Message;
  * @author makejava
  * @since 2024-02-20 22:03:57
  */
+@Mapper
+@Repository
 public interface MessageMapper extends BaseMapper<Message> {
 
 }
