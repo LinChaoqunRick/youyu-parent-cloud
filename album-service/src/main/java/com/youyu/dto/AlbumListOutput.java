224cf7acf084e65d47ae8cc44860bf489c53c2d3
LinChaoqunRick <602314134@qq.com>
2024-06-12 23:01:56 +0800

    feat: 相册列表添加用户信息

--- album-service/src/main/java/com/youyu/dto/AlbumListOutput.java
+++ album-service/src/main/java/com/youyu/dto/AlbumListOutput.java
@@ -1,5 +1,6 @@
 package com.youyu.dto;
 
+import com.youyu.dto.post.post.PostUserOutput;
 import com.youyu.entity.Album;
 import com.youyu.entity.user.User;
 import lombok.Data;
@@ -9,5 +10,6 @@ import java.util.List;
 @Data
 public class AlbumListOutput extends Album {
     private Long imageCount;
+    private PostUserOutput userInfo;
     List<User> authorizedUserList;
 }
--- album-service/src/main/java/com/youyu/service/impl/AlbumServiceImpl.java
+++ album-service/src/main/java/com/youyu/service/impl/AlbumServiceImpl.java
@@ -1,17 +1,28 @@
 package com.youyu.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.youyu.dto.AlbumListOutput;
 import com.youyu.dto.common.PageOutput;
+import com.youyu.dto.post.post.PostUserExtraInfo;
+import com.youyu.dto.post.post.PostUserOutput;
+import com.youyu.entity.post.Post;
+import com.youyu.entity.post.PostLike;
+import com.youyu.entity.user.User;
+import com.youyu.feign.UserServiceClient;
 import com.youyu.mapper.AlbumMapper;
 import com.youyu.entity.Album;
 import com.youyu.service.AlbumService;
+import com.youyu.utils.BeanCopyUtils;
 import com.youyu.utils.PageUtils;
+import com.youyu.utils.SecurityUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * (Album)表服务实现类
@@ -24,6 +35,9 @@ public class AlbumServiceImpl extends ServiceImpl<AlbumMapper, Album> implements
     @Resource
     private AlbumMapper albumMapper;
 
+    @Resource
+    private UserServiceClient userServiceClient;
+
     @Override
     public PageOutput<AlbumListOutput> selectPage(Page<Album> page, String name) {
         Page<Album> albumPage = albumMapper.selectPage(page, name);
@@ -35,8 +49,28 @@ public class AlbumServiceImpl extends ServiceImpl<AlbumMapper, Album> implements
         pageOutput.getList().forEach(item -> {
             item.setAuthorizedUserList(null);
             item.setAuthorizedUsers(null);
+            PostUserOutput detail = getUserDetailById(item.getUserId());
+            item.setUserInfo(detail);
         });
         return pageOutput;
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @param userId 用户id
+     * @return 用户信息
+     */
+    public PostUserOutput getUserDetailById(Long userId) {
+        if (Objects.isNull(userId)) {
+            return null;
+        }
+        User user = userServiceClient.selectById(userId).getData();
+        if (!Objects.isNull(user)) {
+            return BeanCopyUtils.copyBean(user, PostUserOutput.class);
+        } else {
+            return null;
+        }
     }
 }
 
